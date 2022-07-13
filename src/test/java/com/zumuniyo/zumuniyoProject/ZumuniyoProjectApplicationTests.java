48df1b8bd7dddf249fb40911596c6a0434b97e75
JunHee Myung <platinyan@naver.com>
2022-07-13 14:29:10 +0900

    init

---
+++ src/test/java/com/zumuniyo/zumuniyoProject/ZumuniyoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zumuniyo.zumuniyoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZumuniyoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
