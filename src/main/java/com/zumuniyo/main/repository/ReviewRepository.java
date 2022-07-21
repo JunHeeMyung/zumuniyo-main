081d153cba232adfbc62898e15e3c8af97c018c9
Leedun <313lds@naver.com>
2022-07-21 10:01:01 +0900

    Merge branch 'master' of https://github.com/JunHeeMyung/zumuniyo-main

--- src/main/java/com/zumuniyo/main/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zumuniyo.main.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.zumuniyo.main.dto.ReviewDTO;
-
-public interface ReviewRepository extends CrudRepository<ReviewDTO, Long>{
-
-}
