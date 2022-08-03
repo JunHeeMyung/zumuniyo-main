ce4da0b452cfa8607a5f0a473b189314f33fc3de
Leedun <313lds@naver.com>
2022-08-03 09:31:40 +0900

    11

--- src/main/java/com/zumuniyo/main/controller/ReviewController.java
+++ src/main/java/com/zumuniyo/main/controller/ReviewController.java
@@ -3,6 +3,7 @@ package com.zumuniyo.main.controller;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.swing.filechooser.FileSystemView;
 
@@ -21,160 +22,152 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-
+import com.zumuniyo.main.dto.MemberDTO;
 import com.zumuniyo.main.dto.ReviewDTO;
 import com.zumuniyo.main.repository.MemberRepository;
+import com.zumuniyo.main.repository.MenuRepository;
 import com.zumuniyo.main.repository.ReviewRepository;
+import com.zumuniyo.main.repository.ShopRepository;
 
 @RestController
 @RequestMapping("/review/*")
 public class ReviewController {
-	
+
 	@Autowired
 	ReviewRepository reviewRepo;
-	
+
 	@Autowired
 	MemberRepository memRepo;
-	
-//	ReviewDTO upReview = new ReviewDTO();
-	ReviewDTO upReview = ReviewDTO.builder().build();
-	
-	
-	@GetMapping("/reviewList")
-	public List<ReviewDTO> reviewList() {
-		
-//		List<ReviewDTO> reviewList = (List<ReviewDTO>) reviewRepo.findAll();		
-		List<ReviewDTO> reviewList = (List<ReviewDTO>) reviewRepo.findAll(Sort.by(Sort.Direction.DESC,"reviewRegdate"));
-		
-		return reviewList;
-	}
-	
+
+//	@Autowired
+//	ShopRepository shopRepo;	
+//	
+//	@Autowired
+//	MenuRepository menuRepo;
+
+//	ReviewDTO upReview = ReviewDTO.builder().build();
+	List<String> imgs = new ArrayList<>();
+
 	@GetMapping("/reviewTest")
-	public String reviewTest() {		
-		
+	public String reviewTest() {
 		return "도착";
 	}
-	
-	
-	
+
+	@GetMapping("/reviewList")
+	public List<ReviewDTO> reviewList() {
+		List<ReviewDTO> reviewList = (List<ReviewDTO>) reviewRepo
+				.findAll(Sort.by(Sort.Direction.DESC, "reviewRegdate"));
+		return reviewList;
+	}
+
+	// 미구현
+	@GetMapping("/reviewShopList/{shopseq}")
+	public List<ReviewDTO> reviewShopList(@PathVariable Long shopseq) {
+		// 샵의 리뷰 미구현
+		System.out.println("shopseq :" + shopseq);
+//		shopRepo.findById(shopseq);
+
+		List<ReviewDTO> reviewList = (List<ReviewDTO>) reviewRepo
+				.findAll(Sort.by(Sort.Direction.DESC, "reviewRegdate"));
+		return reviewList;
+	}
+
+	// 미구현
+	@GetMapping("/reviewMenuList/{menuseq}")
+	public List<ReviewDTO> reviewMenuList(@PathVariable Long menuseq) {
+		// 메뉴의 리뷰 미구현
+		System.out.println("menuseq :" + menuseq);
+		List<ReviewDTO> reviewList = (List<ReviewDTO>) reviewRepo
+				.findAll(Sort.by(Sort.Direction.DESC, "reviewRegdate"));
+		return reviewList;
+	}
 
 //	@PostMapping(value="/reviewInsert" , consumes = "application/json")	
-	@PostMapping("/reviewInsert")
-	public void reviewInsert(@RequestBody  ReviewDTO review) {	
-				
-		review.setMember(memRepo.findById(1l).get());
+	@PostMapping("/reviewInsert/{memNick}")
+//	@PostMapping("/reviewInsert")
+//	public void reviewInsert(ReviewDTO review) {	
+//	public void reviewInsert(@RequestBody ReviewDTO review, @RequestBody  String memNick) {					
+	public int reviewInsert(ReviewDTO review, @PathVariable String memNick) {
+
+		review.setMember(memRepo.findByMemNick(memNick).get());
+//		review.setMember(memRepo.findById(114l).get());
 		review.setReviewExposure(false);
-		if(upReview!=null) {
-			System.out.println("이미지 있음");
-			review.setReviewImages(upReview.getReviewImages());
-		}
+				
 		
-//		review = ReviewDTO.builder()
-//					.member(memRepo.findById(1l).get())						
-//					.reviewImages(upReview.getReviewImages())
-//					.build();;
-		System.out.println(review);		
-		reviewRepo.save(review);
+		if (imgs != null) {
+			System.out.println("이미지 있음");
+			review.setReviewImages(imgs);
+		}				
+
+
+		System.out.println(review);
+//		reviewRepo.save(review);
+		if (reviewRepo.save(review) != null) {
+			return 1;
+		} else {
+			return -1;
+		}
 	}
-	
-	
+
 	@PutMapping("/reviewUpdate/{bno}")
 	public void reviewUpdate(@PathVariable Long bno, @RequestBody ReviewDTO review) {
-		
+
 		ReviewDTO reviewUpdate = reviewRepo.findById(bno).get();
 		reviewUpdate = review;
 		System.out.println(reviewUpdate);
 //		reviewRepo.save(reviewUpdate);
-		
-	}
-	
-	@DeleteMapping("/reviewDelete/{bno}")
-	public void reviewDelete(@PathVariable Long bno)	{
-		System.out.println("삭제요청옴");
-		ReviewDTO reviewDelete = reviewRepo.findById(bno).get();
-		System.out.println(reviewDelete);
-		
-		reviewRepo.deleteById(bno);
-		
-	}
-	
 
-	//@GetMapping("/upload")
-	//public void reviewImg(String aa) throws Exception {
-	@CrossOrigin(origins = {"http://localhost:3000"})	
+	}
+
+	@DeleteMapping("/reviewDelete/{bno}")
+//	public String reviewDelete(@PathVariable Long bno)	{
+	public String reviewDelete(@PathVariable Long bno, String memNick) {
+
+		System.out.println("삭제요청옴");
+		System.out.println("bno&memnick :" + bno + "&" + memNick);
+		ReviewDTO reviewDelete = reviewRepo.findById(bno).get();
+
+		String reviewnick = reviewDelete.getMember().getMemNick();
+//		MemberDTO deletemem = memRepo.findByMemNick(memnick).get();		
+
+		String dmem = memRepo.findByMemNick(memNick).get().getMemNick();
+//		String dmem = deletemem.getMemNick();
+
+		System.out.println("reviewnick&dmem :" + reviewnick + "&" + dmem);
+
+		System.out.println(reviewDelete);
+
+		if (reviewnick.equals(dmem)) {
+			System.out.println("정보일치");
+			reviewRepo.deleteById(bno);
+			return "success";
+		} else {
+			System.out.println("불일치");
+			return "fail";
+		}
+
+	}
+
+	// @GetMapping("/upload")
+	// public void reviewImg(String aa) throws Exception {
+//	@CrossOrigin(origins = {"http://localhost:3000"})	
 	@PostMapping("/upload")
+//	public void reviewImg(@RequestParam MultipartFile file) throws Exception {
 	public void reviewImg(@RequestParam MultipartFile file) throws Exception {
 		System.out.println("요청들어옴");
-		
 //		String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
-//		String testPath = "C:/MSA/3Project/zumuniyo-main/src/main/resources/img";
-		
-	//	String testPath = "C:/MSA/reacttest/forreact/public";
-		String testPath = "C:/MSA/3Project/zumuniyo-react/public";
 		
 		System.out.println(file);
-		String rootPath = testPath;
-//		String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
-	    String basePath = rootPath + "/" + "img";
-	    String filePath = basePath + "/" + file.getOriginalFilename();
-	    File dest = new File(filePath);
-	    file.transferTo(dest); // 파일 업로드 작업 수행
-	    
-	    List<String> imgs = new ArrayList<>();
-	    imgs.add(filePath);
-	    
-	    
-//	    	upReview = ReviewDTO.builder()
-//	    		.reviewImages(imgs)
-//	    		.build();
-	    	
-	    upReview.setReviewImages(imgs);
-	    System.out.println(upReview);
-	   // reviewRepo.save(reviewImg);
-	   // reviewRepo.save(upReview);
-	   // return file.getOriginalFilename();
-	    
+		String rootPath = "C:/MSA/3Project/zumuniyo-react/public";		
+		String basePath = rootPath + "/" + "img";
+		String filePath = basePath + "/" + file.getOriginalFilename();
+		File dest = new File(filePath);
+		
+		file.transferTo(dest); // 파일 업로드 작업 수행
+		
+		imgs.add(filePath);
+
 	}
-	
-	
-	
-//	@PostMapping("/reviewInsert")
-//	@CrossOrigin(origins = {"http://localhost:3000"})
-//	public void reviewInsert(@RequestBody  ReviewDTO review, @RequestParam MultipartFile file) throws Exception {	
-//				
-//		
-//		if(file!=null) {
-//			System.out.println("업로드파일들어옴");
-//			String testPath = "C:/MSA/3Project/zumuniyo-main/src/main/resources/img";
-//    //  	String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
-//			String rootPath = testPath;
-//		    String basePath = rootPath + "/" + "review";
-//		    String filePath = basePath + "/" + file.getOriginalFilename();
-//		    File dest = new File(filePath);
-//		    file.transferTo(dest); // 파일 업로드 작업 수행
-//		    
-//		    List<String> imgs = new ArrayList<>();
-//		    imgs.add(filePath);
-//		    
-//		    review = ReviewDTO.builder()
-//					.member(memRepo.findById(1l).get())
-//					.reviewImages(imgs)
-//					.build();;
-//			
-//			reviewRepo.save(review);
-//		    
-//			
-//		}else {
-//			System.out.println("업로드파일 없음");
-//			review = ReviewDTO.builder()
-//					.member(memRepo.findById(1l).get())		
-//					.build();;
-//					
-//			System.out.println(review);		
-//			reviewRepo.save(review);		
-//		}	
-//	}
-	
-	
-	
+
+
 }
