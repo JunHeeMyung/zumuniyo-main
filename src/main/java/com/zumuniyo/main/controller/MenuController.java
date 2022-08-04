68ccb44fbf2bfc861c5b08cc7cdd95c4b0dd475a
DESKTOP-LE375VU\yhj <yhju765@gmail.com>
2022-08-04 23:16:09 +0900

    8/4 2316

--- src/main/java/com/zumuniyo/main/controller/MenuController.java
+++ src/main/java/com/zumuniyo/main/controller/MenuController.java
@@ -3,7 +3,6 @@ package com.zumuniyo.main.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,13 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zumuniyo.main.dto.MenuCategoryDTO;
 import com.zumuniyo.main.dto.MenuDTO;
 import com.zumuniyo.main.dto.MenuStatus;
-import com.zumuniyo.main.dto.ShopDTO;
+import com.zumuniyo.main.repository.MenuCategoryRepository;
 import com.zumuniyo.main.repository.MenuRepository;
 import com.zumuniyo.main.repository.ShopRepository;
 
@@ -35,6 +33,9 @@ public class MenuController {
 	
 	@Autowired
 	ShopRepository shopRepository;
+		
+	@Autowired
+	MenuCategoryRepository menuCategoryRepository;
 	
 	String insertResult;
 	
@@ -66,37 +67,73 @@ public class MenuController {
 	}
 	
 	
+	// 메뉴카테고리 조회
+	@GetMapping("/menucategory/{shopseq}")
+	public List<MenuCategoryDTO> menuCategoryList(@PathVariable Long shopseq) {
+		
+		List<MenuCategoryDTO> menuCategoryList = new ArrayList<>();
+		
+		shopRepository.findById(shopseq).ifPresent(shop->{
+			
+			menuCategoryRepository.findByShopAndMenuCategoryOrderNotOrderByMenuCategoryOrderAsc(shop, 0).forEach(sc->{
+				
+				menuCategoryList.add(sc);
+			});
+			
+		});
+		return menuCategoryList;
+	}
+	
+	
+	
+	// 특정 메뉴카테고리 안의 메뉴이름 조회
+	@GetMapping("/menucategoryview/{cno}")
+	
+	public List<MenuDTO> menuSelectByCategory(@PathVariable Long cno) {
+			
+		List<MenuDTO> menuListByCategory = new ArrayList<>();
+		
+		MenuCategoryDTO mc = MenuCategoryDTO.builder().menuCategorySeq(cno).build();
+		
+		menuRepository.findByMenuCategoryOrderByMenuSeq(mc).forEach(menu->{
+			
+			menuListByCategory.add(menu);
+		});
+		return menuListByCategory;
+		
+	}
+	
+	
+	
 	
 	// 메뉴등록페이지
-	
-	/*
-	@GetMapping("/menuInsert")
-	public String menuInsertGet() {
-		
-		return "menu/menuInsert";
-	}
-	*/
-	
-	
 	@PostMapping("/menuInsert/{shopseq}")
-	public String menuInsertPost(@PathVariable Long shopseq , Long cno, MenuDTO menuDTO ) {
+	public String menuInsertPost(@PathVariable Long shopseq , MenuDTO menuDTO ) {
 		
-		ShopDTO shop = ShopDTO.builder().shopSeq(shopseq).build();
+		System.out.println("shopseq :" + shopseq);
+		System.out.println("menuDTO :" + menuDTO);
 		
-		MenuCategoryDTO menuCategory = MenuCategoryDTO.builder().menuCategorySeq(cno).build();
+//		ShopDTO shop = ShopDTO.builder().shopSeq(shopseq).build();
 		
+		MenuCategoryDTO menuCategory = MenuCategoryDTO.builder()
+				.menuCategorySeq(61L)
+				.menuCategoryName("밥류")
+				.menuCategoryOrder(3)
+				.shop(shopRepository.findById(shopseq).get())				
+				.build();		
+		
+		System.out.println(menuCategory);
 		
 		insertResult="";
 		
 		if(menuDTO.getMenuName()==null||menuDTO.getMenuName()=="") return "메뉴 이름을 입력해주세요.";
-			
 		if(!Pattern.matches("^[가-힣]{2,16}$", menuDTO.getMenuName())) return "메뉴 이름은 한글 2~16자로 작성해주세요.";	
 		
 		
 		
 		MenuDTO menu = MenuDTO.builder()
 				
-				.shop(shop)
+				.shop(shopRepository.findById(shopseq).get())
 				.menuCategory(menuCategory)
 				.menuName(menuDTO.getMenuName())
 				.menuPrice(menuDTO.getMenuPrice())
@@ -113,6 +150,74 @@ public class MenuController {
 		
 		return "메뉴 등록이 완료되었습니다.";
 	}
+	
+	
+	// 메뉴삭제(비활성화)
+	@PostMapping("/menuDelete/{menuseq}")
+	public String MenuDeletePost(@PathVariable Long menuseq , MenuDTO menuDTO) {
+
+		menuRepository.findById(menuseq).ifPresent(menu->{
+			
+			menu.setMenuStatus(MenuStatus.비활성);
+			
+			menuRepository.save(menu);
+			
+		});
+		return "메뉴가 삭제되었습니다.";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@PostMapping("/menuInsert/{shopseq}")
+//	public String menuInsertPost(@PathVariable Long shopseq , Long cno, MenuDTO menuDTO ) {
+//		
+//		ShopDTO shop = ShopDTO.builder().shopSeq(shopseq).build();
+//		
+//		MenuCategoryDTO menuCategory = MenuCategoryDTO.builder().menuCategorySeq(cno).build();
+//		
+//		
+//		insertResult="";
+//		
+//		if(menuDTO.getMenuName()==null||menuDTO.getMenuName()=="") return "메뉴 이름을 입력해주세요.";
+//		
+//		if(!Pattern.matches("^[가-힣]{2,16}$", menuDTO.getMenuName())) return "메뉴 이름은 한글 2~16자로 작성해주세요.";	
+//		
+//		
+//		
+//		MenuDTO menu = MenuDTO.builder()
+//				
+//				.shop(shop)
+//				.menuCategory(menuCategory)
+//				.menuName(menuDTO.getMenuName())
+//				.menuPrice(menuDTO.getMenuPrice())
+//				.menuImage(menuDTO.getMenuImage())
+//				.menuTop(menuDTO.isMenuTop())
+//				.menuSimpleInfo(menuDTO.getMenuSimpleInfo())
+//				.menuDetailInfo(menuDTO.getMenuDetailInfo())
+//				.menuStatus(MenuStatus.활성)
+//				.build();
+//		
+//		MenuDTO result = menuRepository.save(menu);
+//		
+//		log.info("[메뉴등록]:" + result.toString());
+//		
+//		return "메뉴 등록이 완료되었습니다.";
+//	}
 	
 	
 	
