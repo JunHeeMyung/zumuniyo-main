b081e34c9565bd164cd870e44c4cbe7b5dd134f3
WONYOUNGHUB <zero2317@naver.com>
2022-08-08 01:59:01 +0900

    Merge branch 'master' of https://github.com/JunHeeMyung/zumuniyo-main

--- src/main/java/com/zumuniyo/main/repository/CouponRepository.java
+++ src/main/java/com/zumuniyo/main/repository/CouponRepository.java
@@ -1,10 +1,15 @@
 package com.zumuniyo.main.repository;
 
+import java.util.List;
+
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.zumuniyo.main.dto.CouponDTO;
+import com.zumuniyo.main.dto.OrderGroupDTO;
 
 public interface CouponRepository extends QuerydslPredicateExecutor<CouponDTO>,PagingAndSortingRepository<CouponDTO, Long>{
 
+	List<CouponDTO> findByOrderGroup(OrderGroupDTO orderGroup);
+	
 }
