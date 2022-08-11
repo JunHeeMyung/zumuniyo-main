41cbd5c4385286b1da72ba8187a3eb521b93b0b8
Leejw97 <wjddn10tp@gmail.com>
2022-08-11 19:29:24 +0900

    Merge branch 'master' into 이정우

--- src/main/java/com/zumuniyo/main/controller/ShopController.java
+++ src/main/java/com/zumuniyo/main/controller/ShopController.java
@@ -1,13 +1,24 @@
 package com.zumuniyo.main.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +26,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.zumuniyo.main.dto.LocationDTO;
 import com.zumuniyo.main.dto.MemberDTO;
@@ -26,6 +38,9 @@ import com.zumuniyo.main.repository.LocationRepository;
 import com.zumuniyo.main.repository.MemberRepository;
 import com.zumuniyo.main.repository.ShopRepository;
 
+import lombok.extern.java.Log;
+
+@Log
 @RestController
 @RequestMapping("/shop/*")
 public class ShopController {
@@ -110,6 +125,91 @@ public class ShopController {
 		
 		return 	(result==null||result.getShopSeq()==null)?"실패":"성공";
 	}
+	
+	//--------------------------------------------------------------------------------
+	@PostMapping("/upload")
+	public List<String> uploadImages( List<MultipartFile> images ,HttpServletRequest request){
+		
+		if(request.getSession().getAttribute("member")==null) return null;
+		String memberSeq = ((MemberDTO)request.getSession().getAttribute("member")).getMemSeq().toString();
+		
+		List<String> fileNames = new ArrayList<String>();
+		
+		try {
+			for(MultipartFile image : images) {
+				
+				String date = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()); 
+				String oldImageName = image.getOriginalFilename();
+				String extension =  oldImageName.indexOf(".")>=0?oldImageName.substring(oldImageName.lastIndexOf(".")+1):"";
+				StringBuilder random = new StringBuilder("");
+				for(int i=0;i<100;i++) random.append(((int)(Math.random()*10)));
+				String newImageName = date+random+memberSeq+(extension.equals("")?"":".")+extension;
+				
+				String rootPath = System.getProperty("user.dir");
+                Path DIR = Paths.get(rootPath+"\\src\\main\\resources\\static\\image");
+				Files.createDirectories(DIR);
+				Path imagePath = DIR.resolve(newImageName).normalize();
+				if(Files.exists(imagePath)) return null;			
+				image.transferTo(imagePath);
+				fileNames.add(newImageName);
+				
+				log.info("[이미지업로드]: "+oldImageName+" => "+newImageName);
+			}
+			
+		}catch(Exception e) {
+			log.info("[이미지업로드] Exception 발생");
+			System.out.println(e);
+			return null;
+		}
+		
+		return fileNames;
+	}
+	
+	@GetMapping(value ="/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<byte[]> getImage(@PathVariable String imageName,HttpServletRequest request){
+
+		try {
+			
+			String rootPath = System.getProperty("user.dir");
+            Path DIR = Paths.get(rootPath+"\\src\\main\\resources\\static\\image");
+			Path imagePath = DIR.resolve(imageName).normalize();
+			if(!Files.exists(imagePath)) return null;
+			
+			InputStream imageStream = new FileInputStream(imagePath.toString());
+			log.info("[이미지요청] "+imagePath.toString());
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			int read;
+			byte[] imageByteArray = new byte[imageStream.available()];
+			while ((read = imageStream.read(imageByteArray, 0, imageByteArray.length)) != -1) {
+				buffer.write(imageByteArray, 0, read);
+			}
+			buffer.flush();
+			byte[] targetArray = buffer.toByteArray();
+			imageStream.close();
+
+			return new ResponseEntity<byte[]>(targetArray, HttpStatus.OK);
+		} catch (Exception e) {
+			log.info("[이미지로딩] Exception 발생");
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	//--------------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	@PutMapping("/shopupdate")
 	public String shopupdate(
@@ -156,9 +256,9 @@ public class ShopController {
 		ShopCategory sc = ShopCategory.valueOf(shopCategory);
 		MemberDTO memberDTO = memberRepository.findById(member).get();
 		ShopStatus ss = ShopStatus.valueOf(shopStatus);
-		Timestamp sr = Timestamp.valueOf(shopRegdate.replace("T", " ").substring(0, 22));
-		
-	
+		//Timestamp sr = Timestamp.valueOf(shopRegdate.replace("T", " ").substring(0, 22));
+		 //long=>timestamp 
+		Timestamp sr = new Timestamp(Long.valueOf(shopRegdate));
 		
 		LocationDTO locationDTO = locationRepository.findById(locAddr).get();
 		if(locationDTO ==null) {
